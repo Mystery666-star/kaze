e37cade60cc729c38ba8a94df3b9b3f9380c3afc
Mystery666-star <parthcr1@gmail.com>
2025-02-15 17:12:45 +0530

    Initial commit

--- src/AccessGranted.js
+++ src/AccessGranted.js
@@ -4,14 +4,15 @@ const AccessGranted = () => {
             style={{
                 position: "relative",
                 display: "flex",
+                flexDirection: "column",
                 justifyContent: "center",
                 alignItems: "center",
-                flexDirection: "column",
                 height: "100vh",
                 width: "100vw",
                 backgroundColor: "black",
                 overflow: "hidden",
-                textAlign: "center" // Ensures everything is properly centered
+                textAlign: "center",
+                padding: "10px",
             }}
         >
             {/* PNG Image */}
@@ -19,40 +20,39 @@ const AccessGranted = () => {
                 src="/Kaze.png" 
                 alt="Background" 
                 style={{
-                    maxWidth: "90%", // Ensures it fits smaller screens
-                    maxHeight: "70vh",
-                    objectFit: "contain"
+                    width: "90%", // Ensures it scales properly on all devices
+                    maxWidth: "400px", // Prevents it from becoming too large
+                    height: "auto",
                 }} 
             />
 
-            {/* GIF Image Positioned Over PNG */}
+            {/* GIF Image - Positioned over PNG */}
             <img 
                 src="/comingsoon.gif" 
                 alt="Coming Soon" 
                 style={{
                     position: "absolute",
-                    width: "35%", // Adjusted for better scaling
-                    maxWidth: "300px",
-                    maxHeight: "30vh",
-                    top: "55%",
-                    left: "50%",
-                    transform: "translate(-50%, -50%)"
+                    width: "50vw", // Dynamic size for all screen sizes
+                    maxWidth: "200px", // Prevents it from getting too large
+                    top: "45%", // Adjusted for better centering
+                    transform: "translateY(-50%)",
                 }} 
             />
 
             {/* Watch Out Text */}
             <h1 
                 style={{
-                    position: "absolute",
-                    bottom: "5%", // Positioned a bit higher for mobile
-                    fontSize: "2rem",
+                    marginTop: "20px", // Adds spacing between images and text
+                    fontSize: "4.5vw", // Responsive text
                     fontWeight: "bold",
                     color: "#ffcc00",
                     textShadow: "3px 3px 10px rgba(255, 255, 255, 0.8)",
                     letterSpacing: "2px",
+                    width: "90%",
+                    maxWidth: "400px",
+                    whiteSpace: "normal", // Allows text to wrap on smaller screens
                     textAlign: "center",
-                    width: "90%", // Ensures it doesn't overflow on mobile
-                    animation: "pulseText 1.5s infinite alternate"
+                    animation: "pulseText 1.5s infinite alternate",
                 }}
             >
                 WATCH OUT THIS SPACE FOR MORE ACTION
